ceb2ce95773c60ebd49979e0db3c509f1d2ebaa1
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-11-06 12:20:33 +0200

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+export const routes: Routes = [{ path: '**', component: WelcomeComponent }];
