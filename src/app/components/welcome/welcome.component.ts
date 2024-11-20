8a6bcac14b9d1e7e226f36dbb94b676514acb58d
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-11-20 15:11:39 +0200

    UI updates

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-welcome',
-  standalone: true,
-  imports: [],
-  templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+    selector: 'app-welcome',
+    standalone: true,
+    imports: [],
+    templateUrl: './welcome.component.html',
+    styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
-
+    impetusFunding =
+        'The IMPETUS project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement No 101037084 Funded in the EU Horizon 2020 Green Deal call';
 }
