7cbfd4e2b4450719f52cd678765057db92361dbb
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-12-11 12:30:01 +0200

    added rcrft

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-welcome',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './welcome.component.html',
     styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
     impetusFunding =
         'The IMPETUS project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement No 101037084 Funded in the EU Horizon 2020 Green Deal call';
+
+    onUnderConstructionClick() {
+        alert('Under Construction');
+    }
 }
